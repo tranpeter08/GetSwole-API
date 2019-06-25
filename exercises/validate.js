8e03db2040782d90049e2fd493d5f084fc83adfd
Peter Tran <tran.peter08@gmail.com>
2019-06-24 17:03:26 -0700

    fixed exercise validation bug for resistUnit input

--- exercises/validate.js
+++ exercises/validate.js
@@ -3,7 +3,7 @@
 exports.validateExercise = function(req, res, next) {
   const {resistUnit} = req.body;
 
-  const correctUnits = ['lb', 'kg'];
+  const correctUnits = ['lb', 'kg', 'other'];
 
   if (!correctUnits.includes(resistUnit)) {
     return res.status(400).json({message: 'Incorrect resistance unit'});
